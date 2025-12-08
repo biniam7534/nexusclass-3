8d0a3dba8fe3b2c6266bfbb63db0eb947885acfa
Biniam <biniamabuu75@gmail.com>
2025-12-08 16:16:32 +0300

    initial

---
+++ classB/indexB.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json())
+app.get("/" ,(req, res) => {res.json({message: "wellcome to" +PORT});
+
+});
