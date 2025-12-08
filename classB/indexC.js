61b1ab6666acd8124d309af04e4c57a6719e7632
Biniam <biniamabuu75@gmail.com>
2025-12-08 23:15:10 +0300

    initial

--- classB/indexC.js
+++ classB/indexC.js
@@ -10,6 +10,21 @@ app.use(express.json())
 app.get("/items" ,(req, res) => {res.json({message: "fetching all items" });
 
 });
+app.post("/items", (req, res)=>{
+    const data = req.body;
+    res.json({messsage: "items created", item:data})
+})
+
+app.patch("/items/:id",(req, res)=> {
+    const { id } = req.params;
+    const data =req.body;
+    res.json({message: `items ${id} updated`, updatedData: req.body})
+})
+app.delete("/items/:id", (req,res)=> {
+    const {id} = req.params;
+    res.json ({message: `items ${id} deleted`})
+})
+
 app.listen(PORT, ()=>{
     console.log(`running of the port ${PORT}`)
 })
